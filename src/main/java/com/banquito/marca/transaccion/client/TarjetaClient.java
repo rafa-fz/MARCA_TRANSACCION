3d0a33228203ef14b779ea940d3c5986e77f5b13
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-03-04 12:29:01 -0500

    Update TarjetaClient.java

--- src/main/java/com/banquito/marca/transaccion/client/TarjetaClient.java
+++ src/main/java/com/banquito/marca/transaccion/client/TarjetaClient.java
@@ -1,9 +1,11 @@
 package com.banquito.marca.transaccion.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import com.banquito.marca.transaccion.controller.dto.SwiftResponseDTO;
 import com.banquito.marca.transaccion.controller.dto.ValidacionTarjetaRequestDTO;
 import com.banquito.marca.transaccion.controller.dto.ValidacionTarjetaResponseDTO;
 
@@ -12,4 +14,7 @@ public interface TarjetaClient {
     
     @PostMapping("/api/v1/tarjetas/validar")
     ValidacionTarjetaResponseDTO validarTarjeta(@RequestBody ValidacionTarjetaRequestDTO request);
-} 
+
+    @GetMapping("/api/v1/tarjetas/numero/{numeroTarjeta}/swift")
+    SwiftResponseDTO obtenerSwift(@PathVariable("numeroTarjeta") String numeroTarjeta);
+}
