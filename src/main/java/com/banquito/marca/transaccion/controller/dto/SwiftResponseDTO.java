0d7933b80d8b7d3a2b7addc0f249b55e32f115f6
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-03-04 12:29:10 -0500

    Create SwiftResponseDTO.java

---
+++ src/main/java/com/banquito/marca/transaccion/controller/dto/SwiftResponseDTO.java
@@ -0,0 +1,13 @@
+package com.banquito.marca.transaccion.controller.dto;
+
+public class SwiftResponseDTO {
+    private String swiftBanco;
+
+    public String getSwiftBanco() {
+        return swiftBanco;
+    }
+
+    public void setSwiftBanco(String swiftBanco) {
+        this.swiftBanco = swiftBanco;
+    }
+}
