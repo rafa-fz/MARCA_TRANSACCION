cbf84bd92d71321f3337d0898340c7094bcb0c61
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-03-04 12:29:23 -0500

    Update TransaccionPeticionMapper.java

--- src/main/java/com/banquito/marca/transaccion/controller/mapper/TransaccionPeticionMapper.java
+++ src/main/java/com/banquito/marca/transaccion/controller/mapper/TransaccionPeticionMapper.java
@@ -1,19 +1,40 @@
 package com.banquito.marca.transaccion.controller.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
 import com.banquito.marca.transaccion.controller.dto.TransaccionPeticionDTO;
 import com.banquito.marca.transaccion.model.Transaccion;
 
-@Mapper(componentModel = "spring")
-public interface TransaccionPeticionMapper {
-    
-    @Mapping(target = "fechaRecepcion", ignore = true)
-    @Mapping(target = "fechaRespuesta", ignore = true)
-    @Mapping(target = "estado", ignore = true)
-    @Mapping(target = "transaccionEncriptada", ignore = true)
-    Transaccion toPersistance(TransaccionPeticionDTO dto);
+@Component
+public class TransaccionPeticionMapper {
 
-    TransaccionPeticionDTO toDTO(Transaccion transaccion);
+    public Transaccion toPersistance(TransaccionPeticionDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Transaccion transaccion = new Transaccion();
+        transaccion.setCodigoUnicoTransaccion(dto.getCodigoUnicoTransaccion());
+        transaccion.setNumeroTarjeta(dto.getNumeroTarjeta());
+        transaccion.setCvv(dto.getCvv());
+        transaccion.setFechaCaducidad(dto.getFechaCaducidad());
+        transaccion.setMonto(dto.getMonto());
+        
+        return transaccion;
+    }
+
+    public TransaccionPeticionDTO toDto(Transaccion entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        TransaccionPeticionDTO dto = new TransaccionPeticionDTO();
+        dto.setCodigoUnicoTransaccion(entity.getCodigoUnicoTransaccion());
+        dto.setNumeroTarjeta(entity.getNumeroTarjeta());
+        dto.setCvv(entity.getCvv());
+        dto.setFechaCaducidad(entity.getFechaCaducidad());
+        dto.setMonto(entity.getMonto());
+        
+        return dto;
+    }
 }
